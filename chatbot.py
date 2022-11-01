c5d352454ad55d365579dfdb315dbd8380583cd5
xingangli <xingang.li@utexas.edu>
2022-11-01 16:55:10 -0500

    update

--- chatbot.py
+++ chatbot.py
@@ -13,8 +13,7 @@ import os
 import datetime
 import numpy as np
 from pathlib import Path
-
-
+            
 # Build the AI
 class ChatBot():
     def __init__(self, name):
@@ -34,7 +33,7 @@ class ChatBot():
     
     def take_user_input(self):
         self.text = input("Enter you question: ")
-
+                 
     @staticmethod
     def text_to_speech(text):
         current_dir = Path(__file__).resolve().parent
@@ -42,7 +41,7 @@ class ChatBot():
         print("ai --> ", text)
         speaker = gTTS(text=text, lang="en", slow=False)
         speaker.save(audio_dir)
-        os.system(f"start {audio_dir}")  #mac->afplay | windows->start
+        os.system(f"rhythmbox-client --play {audio_dir}")  #mac->afplay | windows->start | linux -> mgp123(need installation)
         # os.remove("res.mp3")
 
     def wake_up(self, text):
@@ -51,13 +50,13 @@ class ChatBot():
     @staticmethod
     def action_time():
         return datetime.datetime.now().time().strftime('%H:%M')
-
-
+    
+   
 # Run the AI
 if __name__ == "__main__":
     
-    ai = ChatBot(name="maya")
-    nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
+    ai = ChatBot(name="groot")
+    nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium") #will download the model for the first use
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
     shut_down = ["close yourself", "shut down", "go away", "turn off"]
@@ -69,11 +68,11 @@ if __name__ == "__main__":
 
         ## wake up
         if ai.wake_up(ai.text) is True:
-            res = "Hello I am Maya the AI, what can I do for you?"
+            res = "Hello I am Groot the AI, what can I do for you?"
         
-        ## action time
-        # elif "time" in ai.text:
-        #     res = ai.action_time()
+        # action time
+        elif "time" in ai.text:
+            res = ai.action_time()
         
         ## respond politely
         elif any(i in ai.text for i in ["thank","thanks"]):
@@ -83,10 +82,10 @@ if __name__ == "__main__":
             res = np.random.choice(["See you.", "Bye!"])
             on_off = False
         ## conversation
-        else:   
-            chat = nlp(transformers.Conversation(ai.text), pad_token_id=50256)
+        else:
+            chat = nlp(transformers.Conversation(ai.text), pad_token_id=50256) #use the NLP model
             res = str(chat)
             res = res[res.find("bot >> ")+6:].strip()
-
+        
         ai.text_to_speech(res)
 
